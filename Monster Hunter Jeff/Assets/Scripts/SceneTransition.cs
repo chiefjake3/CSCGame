303121dc456a7594029199215150f5fdf1c3fff0
IssacI14 <springrex37@gmail.com>
2020-01-28 20:20:46 -0500

    Added scene transitions

--- Monster Hunter Jeff/Assets/Scripts/SceneTransition.cs
+++ Monster Hunter Jeff/Assets/Scripts/SceneTransition.cs
@@ -9,21 +9,40 @@ public class SceneTransition : MonoBehaviour {
     public Vector2 playerPosition;
     public VectorValue playerStorage;
     public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
 
     private void Awake()
     {
-        if(fadeInPanel != null)
+        if (fadeInPanel != null)
         {
-
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player") && !other.isTrigger)
+        if (other.CompareTag("Player") && !other.isTrigger)
         {
             playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(FadeCo());
+            //SceneManager.LoadScene(sceneToLoad);
         }
     }
+
+    public IEnumerator FadeCo()
+    {
+        if(fadeOutPanel != null)
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        while(!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+
 }
